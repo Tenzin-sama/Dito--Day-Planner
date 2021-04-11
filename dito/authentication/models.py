4ae1e11928201ae0479ba1ec028a6d05c9db90cd
Tenzin Tsering Shrestha <tenzin.zeta@gmail.com>
2021-04-11 16:17:52 +0545

    added models and migrations

--- dito/authentication/models.py
+++ dito/authentication/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import random
+
 
 # Create your models here.
+class User(AbstractUser):
+
+    
+    # Boolean Fields for determining user types
+    USER_CHOICES=(
+        (1,"Admin"),
+        (2,"Personal")
+    )
+
+
+
+    user_type=models.PositiveIntegerField(choices=USER_CHOICES,default=2)
+
+    # user_color= models.CharField(max_length=20,default="%06x" % random.randint(0, 0xFFFFFF))
+
+    # String representation of the User model
+    def __str__(self):
+        return self.username
